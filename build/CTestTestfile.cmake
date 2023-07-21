2b91ab6ea28f71c7ec6a08526dea19a5648a4d9c
rmy-company <18829040656@163.com>
2023-07-21 09:44:03 +0800

    init catkin ws

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rmy/base_learn/ros_learn_ws/src
+# Build directory: /home/rmy/base_learn/ros_learn_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
